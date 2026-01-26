d6c51e892ceb18e981681b17c4cf75e07d27703a
Roman <romazan.26@gmail.com>
2026-01-26 11:28:47 +0300

    Initial Commit

---
+++ 71NeuroSprintPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  71NeuroSprintPro
+//
+//  Created by Роман Главацкий on 26.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
